c63b8cb3d4f9edfafba8a3eb2b9b25a25b3060ea
ravenmoon13 <anasites68@gmail.com>
2026-09-15 12:12:52 -0300

    Update script.js

--- script.js
+++ script.js
@@ -36,7 +36,7 @@ function processarEnvio() {
     }
 
     // 2. Envia para a API do SheetDB usando JSON (Substituiu o PHP/XAMPP)
-    fetch('https://sheetdb.io/api/v1/mzyiz4ylvtalq', {
+    fetch('https://sheetdb.io/api/v1/xws4dr2lbvt2s', {
         method: 'POST',
         headers: {
             'Accept': 'application/json',
